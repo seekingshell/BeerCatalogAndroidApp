ccfa36e869d33cc8d19f5b8e31c33dd48a1058ed
Michelle Tyler <michelletyler@Michelles-MBP.fios-router.home>
2018-01-11 12:23:46 -0800

    Initial commit

---
+++ app/src/main/java/unisys/visionapi/MainActivity.java
@@ -0,0 +1,166 @@
+package unisys.visionapi;
+
+// my packages
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.View;
+import java.io.*;
+import android.os.Environment;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.util.Log;
+import android.net.Uri;
+import android.support.v4.content.FileProvider;
+
+// packages for vision API code
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Block;
+import com.google.cloud.vision.v1.ColorInfo;
+import com.google.cloud.vision.v1.CropHint;
+import com.google.cloud.vision.v1.CropHintsAnnotation;
+import com.google.cloud.vision.v1.DominantColorsAnnotation;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.FaceAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.ImageSource;
+import com.google.cloud.vision.v1.LocationInfo;
+import com.google.cloud.vision.v1.Page;
+import com.google.cloud.vision.v1.Paragraph;
+import com.google.cloud.vision.v1.SafeSearchAnnotation;
+import com.google.cloud.vision.v1.Symbol;
+import com.google.cloud.vision.v1.TextAnnotation;
+import com.google.cloud.vision.v1.WebDetection;
+import com.google.cloud.vision.v1.WebDetection.WebEntity;
+import com.google.cloud.vision.v1.WebDetection.WebImage;
+import com.google.cloud.vision.v1.WebDetection.WebPage;
+import com.google.cloud.vision.v1.Word;
+import com.google.protobuf.ByteString;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    VisionAPINetwork vap;
+
+    public void dispatchTakePictureIntent(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.d("VisionAPICamera", "failed to create directory");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+                // start the vision API analysis
+                //visionApiAnalysis(photoURI);
+                //String photoUriStr = photoURI.toString();
+                    vap = new VisionAPINetwork();
+                    vap.execute(photoURI);
+                    //vap.detectLogos(photoURI);
+            }
+        }
+    }
+
+    /*private void visionApiAnalysis(Uri photoURI) {
+        Vision.Builder visionBuilder = new Vision.Builder(
+                new NetHttpTransport(),
+                new AndroidJsonFactory(),
+                null);
+
+        visionBuilder.setVisionRequestInitializer(
+                new VisionRequestInitializer("AIzaSyDe0cnHOfe53K1oiTVNrLkcxk5o4ea2H4g"));
+
+        Vision vision = visionBuilder.build();
+    }*/
+
+    String mCurrentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    class VisionAPINetwork extends AsyncTask<Uri, Void, Void> {
+        protected void doInBackground(Uri filePath) {
+            try {
+                detectLogos(filePath);
+            }
+            catch(java.lang.Exception e) {
+                Log.d("VisionAPICamera", e.toString());
+            }
+        }
+
+        public void detectLogos(Uri filePath) throws Exception, IOException {
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+
+            //ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+            ByteString imgBytes = ByteString.readFrom(getApplicationContext().getContentResolver().openInputStream(filePath));
+
+            Image img = Image.newBuilder().setContent(imgBytes).build();
+            Feature feat = Feature.newBuilder().setType(Type.LOGO_DETECTION).build();
+            AnnotateImageRequest request =
+                    AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+            requests.add(request);
+
+            try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+                BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+                List<AnnotateImageResponse> responses = response.getResponsesList();
+
+                for (AnnotateImageResponse res : responses) {
+                    if (res.hasError()) {
+                        Log.d("VisionAPICamera", res.getError().getMessage());
+                        return;
+                    }
+
+                    // For full list of available annotations, see http://g.co/cloud/vision/docs
+                    for (EntityAnnotation annotation : res.getLogoAnnotationsList()) {
+                        Log.d("VisionAPICamera", annotation.getDescription());
+                    }
+                }
+            }
+        }
+    }
+}
+
